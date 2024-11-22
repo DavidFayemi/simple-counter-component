a869f1dfecb7606a5478527ee9956f01bb7cd947
David Fayemi <idfayemi@gmail.com>
2024-11-22 04:40:46 +0100

    Done

---
+++ src/components/Counter.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  const handleChangeCount = () => {
+    setCount(count + 1);
+  };
+  const handleResetCount = () => {
+    setCount(0);
+  };
+  return (
+    <div className="count-container">
+      <h1>Counter</h1>
+      <h2 className="count-output">{count}</h2>
+      <div className="count-buttons">
+        <button className="count-button" onClick={handleChangeCount}>
+          Count
+        </button>
+        <button
+          className="count-button count-button--red"
+          onClick={handleResetCount}
+        >
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Counter;
